d1a270c40936bf19f1201db66220d599ca5b4fe0
HanJaeseung <63630187+HanJaeseung@users.noreply.github.com>
2020-04-17 15:34:46 +0900

    Update ingressregistry.go

--- ingressregistry/ingressregistry.go
+++ ingressregistry/ingressregistry.go
@@ -9,13 +9,11 @@ import (
 
 var lock sync.RWMutex
 
-// Common errors.
 var (
 	ErrServiceNotFound = errors.New("service name/version not found")
 )
 
-// Registry is an interface used to lookup the target host
-// for a given service name / version pair.
+
 type Registry interface {
 	Add(host, path, endpoint string)                // Add an endpoint to our registry
 	Delete(host, path, endpoint string)             // Remove an endpoint to our registry
@@ -23,21 +21,8 @@ type Registry interface {
 	Lookup(host, path string) ([]string, error)     // Return the endpoint list for the given service name/version
 }
 
-// DefaultRegistry is a basic registry using the following format:
-// {
-//   "Host": {
-//     "Path": [
-//       "cluster1",
-//       "cluster2"
-//     ],
-//   },
-// }
-
-//type DefaultRegistry map[string]map[string]map[string]string
 type DefaultRegistry map[string]map[string][]string
 
-
-// Lookup return the endpoint list for the given service name/version.
 
 func (r DefaultRegistry) Lookup(host string, path string) ([]string, error) {
 	fmt.Println("----Lookup----")
@@ -54,8 +39,6 @@ func (r DefaultRegistry) Lookup(host string, path string) ([]string, error) {
 
 
 func (r DefaultRegistry) Failure(host, path, endpoint string, err error) {
-	// Would be used to remove an endpoint from the rotation, log the failure, etc.
-	//log.Printf("Error accessing %s/%s (%s): %s", path, endpoint, err)
 	log.Printf("Error accessing %s %s (%s): %s", host, path, endpoint, err)
 }
 
@@ -73,7 +56,6 @@ func (r DefaultRegistry) Add(host, path, endpoint string) {
 }
 
 
-// Delete removes the given endpoit for the service name/version.
 func (r DefaultRegistry) Delete(host, path, endpoint string) {
 	fmt.Println("----Delete----")
 	lock.Lock()
