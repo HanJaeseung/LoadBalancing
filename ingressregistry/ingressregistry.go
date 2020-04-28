c9316a83b1603f2df3b68a37e1d32af9c18b2b3b
HanJaeseung <63630187+HanJaeseung@users.noreply.github.com>
2020-04-28 16:41:24 +0900

    Update ingressregistry.go

--- ingressregistry/ingressregistry.go
+++ ingressregistry/ingressregistry.go
@@ -1,3 +1,4 @@
+// Package registry defines the Registry interface which can be used with goproxy.
 package ingressregistry
 
 import (
@@ -9,11 +10,13 @@ import (
 
 var lock sync.RWMutex
 
+// Common errors.
 var (
 	ErrServiceNotFound = errors.New("service name/version not found")
 )
 
-
+// Registry is an interface used to lookup the target host
+// for a given service name / version pair.
 type Registry interface {
 	Add(host, path, endpoint string)                // Add an endpoint to our registry
 	Delete(host, path, endpoint string)             // Remove an endpoint to our registry
@@ -21,13 +24,24 @@ type Registry interface {
 	Lookup(host, path string) ([]string, error)     // Return the endpoint list for the given service name/version
 }
 
+// DefaultRegistry is a basic registry using the following format:
+// {
+//   "Host": {
+//     "Path": [
+//       "cluster1",
+//       "cluster2"
+//     ],
+//   },
+// }
+
+//type DefaultRegistry map[string]map[string]map[string]string
 type DefaultRegistry map[string]map[string][]string
 
 
+// Lookup return the endpoint list for the given service name/version.
+
 func (r DefaultRegistry) Lookup(host string, path string) ([]string, error) {
 	fmt.Println("----Lookup----")
-	fmt.Println(host)
-	fmt.Println(path)
 	lock.RLock()
 	targets, ok := r[host][path]
 	lock.RUnlock()
@@ -39,6 +53,8 @@ func (r DefaultRegistry) Lookup(host string, path string) ([]string, error) {
 
 
 func (r DefaultRegistry) Failure(host, path, endpoint string, err error) {
+	// Would be used to remove an endpoint from the rotation, log the failure, etc.
+	//log.Printf("Error accessing %s/%s (%s): %s", path, endpoint, err)
 	log.Printf("Error accessing %s %s (%s): %s", host, path, endpoint, err)
 }
 
@@ -56,6 +72,7 @@ func (r DefaultRegistry) Add(host, path, endpoint string) {
 }
 
 
+// Delete removes the given endpoit for the service name/version.
 func (r DefaultRegistry) Delete(host, path, endpoint string) {
 	fmt.Println("----Delete----")
 	lock.Lock()
